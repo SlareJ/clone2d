e84a10198341452e1da25350e90b0ca71f3fe88f
Slare_J <ya.slarej@ya.ru>
2024-03-23 16:39:03 +0300

    Implemented a ScreenWrap for bullets

--- Assets/Scripts/InputSystem/BulletLogic.cs
+++ Assets/Scripts/InputSystem/BulletLogic.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class BulletData
-{
-    public GameObject bullet;
-    public Vector3 direction;
-    public float lifeTime;
 
-    public BulletData(GameObject bullet, Vector3 direction, float lifeTime)
-    {
-        this.bullet = bullet;
-        this.direction = direction;
-        this.lifeTime = lifeTime;
-    }
-
-}
 public class BulletLogic
 {
+    private class BulletData
+    {
+        public GameObject bullet;
+        public Vector3 direction;
+        public float lifeTime;
+
+        public BulletData(GameObject bullet, Vector3 direction, float lifeTime)
+        {
+            this.bullet = bullet;
+            this.direction = direction;
+            this.lifeTime = lifeTime;
+        }
+
+    }
     private float _velocity;
     private List<BulletData> _bullets = new List<BulletData>();
 
@@ -39,11 +40,24 @@ public class BulletLogic
                 GameObject.Destroy(_bullets[i].bullet);
                 _bullets.RemoveAt(i);
             }
+            ScreenWrap(_bullets[i].bullet);
         }
     }
 
     public void AddBullet(GameObject bullet, Vector3 direction, float lifeTime)
     {
         _bullets.Add(new BulletData(bullet, direction, lifeTime));
+    }
+
+    private void ScreenWrap(GameObject bullet)
+    {
+        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(bullet.transform.position);
+
+        if (viewportPosition.x < 0) viewportPosition.x = 1;
+        if (viewportPosition.x > 1) viewportPosition.x = 0;
+        if (viewportPosition.y < 0) viewportPosition.y = 1;
+        if (viewportPosition.y > 1) viewportPosition.y = 0;
+
+        bullet.transform.position = Camera.main.ViewportToWorldPoint(viewportPosition);
     }
 }
